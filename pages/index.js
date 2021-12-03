82e8ff56e9c33101b1a0f8493cb5307484bd23ea
Ara Tepanyan <tepanyan95@gmail.com>
2021-12-03 12:43:48 +0400

    US-1w8mk0w: Change framework > fixed routes, added notification manager [fixed deployment error]

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,10 @@
 import React from "react";
 
-// BASE COMPONENTS
-import Notification from "components/notification/Notification.component";
-// SLICES
-import { setGlobalMessage } from "redux/common/common.slice";
-// EFFECTS
-import useToolkit from "effects/useToolkit.effect";
 // PAGES
 import FeedPage from "pages/feed/index";
 
 const MainPage = () => {
-  const {
-    dispatch,
-    reduxStore: { common: commonStore },
-  } = useToolkit("common");
-
-  const { globalMessage, isAppFirstLaunch } = commonStore;
-
-  const handleNotificationClose = () => {
-    dispatch(setGlobalMessage(null));
-  };
-
-  return (
-    <>
-      {/*<Notification*/}
-      {/*  message={globalMessage?.text}*/}
-      {/*  isOpened={!!globalMessage}*/}
-      {/*  delay={7000}*/}
-      {/*  severity={globalMessage?.severity}*/}
-      {/*  handleNotificationClose={handleNotificationClose}*/}
-      {/*  position={{ vertical: "top", horizontal: "center" }}*/}
-      {/*/>*/}
-      <FeedPage />
-    </>
-  );
+  return <FeedPage />;
 };
 
 MainPage.getInitialProps = async () => {
